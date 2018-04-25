596b82991ef0a404e9e54450fdd81ce5a4453f41
Unknown <Pepak16@student.sdu.dk>
2018-04-26 01:09:42 +0200

    Homework 1 on 3_Agents works!
    but homework 2 not working properly for now..

--- 3_Agents/reflex_vacuum_agent.py
+++ 3_Agents/reflex_vacuum_agent.py
@@ -15,10 +15,14 @@ Environment = {
 def REFLEX_VACUUM_AGENT(loc_st):  # Determine action
     if loc_st[1] == 'Dirty':
         return 'Suck'
-    if loc_st[0] == A:
+    elif loc_st[0] == A:
         return 'Right'
-    if loc_st[0] == B:
+    elif loc_st[0] == B:
+        return 'Down'
+    elif loc_st[0] == C:
         return 'Left'
+    elif loc_st[0] == D:
+        return 'Up'
 
 
 def Sensors():  # Sense Environment
@@ -30,10 +34,26 @@ def Actuators(action):  # Modify Environment
     location = Environment['Current']
     if action == 'Suck':
         Environment[location] = 'Clean'
+    # From position A to B and B back to A
     elif action == 'Right' and location == A:
         Environment['Current'] = B
     elif action == 'Left' and location == B:
         Environment['Current'] = A
+    # From position B to C and C back to B
+    elif action == 'Down' and location == B:
+        Environment['Current'] = C
+    elif action == 'Up' and location == C:
+        Environment['Current'] = B
+    # From position C to D and D back to C
+    elif action == 'Left' and location == C:
+        Environment['Current'] = D
+    elif action == 'Right' and location == D:
+        Environment['Current'] = C
+    # From position D to A and A back to D
+    elif action == 'Up' and location == D:
+        Environment['Current'] = A
+    elif action == 'Down' and location == A:
+        Environment['Current'] = D
 
 
 def run(n):  # run the agent through n steps
@@ -49,4 +69,4 @@ def run(n):  # run the agent through n steps
 
 
 if __name__ == '__main__':
-    run(10)
+    run(20)
